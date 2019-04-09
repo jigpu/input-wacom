762fed0419814c090ccd52d901c69a485e51c65c
Jason Gerecke <jason.gerecke@wacom.com>
2019-04-09 13:47:29 -0700

    travis: Add additional build testing
    Add several new checks to be performed by Travis on each build:
    
      * Can we create and build a source tarball?
      * Does 'make install' work?
      * Does 'make uninstall' work?
      * Does 'make clean' work?
    
    Signed-off-by: Jason Gerecke <jason.gerecke@wacom.com>

---
+++ .travis.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e -x
+
+CONFIG_FLAGS="--with-kernel=${KERNELDIR}/${KERNEL}"
+EXTRA_CFLAGS="-Werror -Wno-type-limits -Wno-unused-function -isystem ${KERNELDIR}/${KERNEL}/arch/x86/include -isystem ${KERNELDIR}/${KERNEL}/arch/x86/include/uapi -isystem ${KERNELDIR}/${KERNEL}/include/linux"
+
+# Try a developer build
+./autogen.sh $CONFIG_FLAGS
+make EXTRA_CFLAGS="$EXTRA_CFLAGS"
+sudo make install
+sudo make uninstall
+make clean
+
+# Try to create and build a source tarball
+make dist
+tar xvfj *.bz2
+rm *.bz2
+cd input-wacom-*
+./configure $CONFIG_FLAGS
+make EXTRA_CFLAGS="$EXTRA_CFLAGS"
+sudo make install
+sudo make uninstall
+make clean
